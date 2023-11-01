730165c06209811b5edc9258be9860966dcb9460
Brandon H. Goding <brandon@patracompany.com>
2023-10-31 23:13:04 -0400

    feat(htmx): implemented html

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ import website.views as website_views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="website/home_page.html"), name="home_page"),
-    path("partials/empty", website_views.empty_route, name="empty_route")
+    path("partials/empty", website_views.empty_route, name="empty_route"),
+    path("partials/webcam", website_views.webcam_partial, name="webcam_modal"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def empty_route(request):
     return HttpResponse('')
+
+
+def webcam_partial(request):
+    return render(request, 'website/partials/webcam_modal_partial.html')
