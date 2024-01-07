285c2269e2b0512211022f7202e5425ea82a173f
Brandon H. Goding <brandon@patracompany.com>
2024-01-06 22:42:29 -0500

    feature(signup): signup form

--- app/membership/models.py
+++ app/membership/models.py
@@ -2,6 +2,9 @@ from datetime import timezone
 
 from django.db import models
 
+from django.contrib.auth.models import User
+User._meta.get_field('email')._unique = True
+
 
 class MembershipTypeChoices(models.TextChoices):
     ADULT = "AD", "Adult"
--- app/www_fortkentoc_org/urls.py
+++ app/www_fortkentoc_org/urls.py
@@ -22,9 +22,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path("", include("website.urls")),
+    path("memberships/", include("membership.urls")),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("password-reset/", auth_views.PasswordResetView.as_view(), name='password_reset'),
