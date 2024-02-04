4232a656a4e5f87885680352983bcef38b449a59
Brandon H. Goding <brandon.h.goding@gmail.com>
2024-02-03 23:47:52 -0500

    refactor(hooks): remove testimonial hook

--- app/website/wagtail_hooks.py
+++ app/website/wagtail_hooks.py
@@ -2,7 +2,7 @@ from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
-from website.models import BoardMember, Coach, EventPage, Testimonial
+from website.models import BoardMember, Coach, EventPage
 
 
 class BoardMemberViewSet(SnippetViewSet):
@@ -37,15 +37,7 @@ class EventModelAdmin(ModelAdmin):
     search_fields = ("title", "date")
 
 
-class TestimonialViewSet(SnippetViewSet):
-    model = Testimonial
-    icon = "openquote"
-    inspect_view_enabled = True
-    add_to_admin_menu = True
-    menu_order = 100
-
 
 register_snippet(BoardMemberViewSet)
 register_snippet(CoachViewSet)
-register_snippet(TestimonialViewSet)
 modeladmin_register(EventModelAdmin)
