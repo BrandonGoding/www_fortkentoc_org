a527b48aee8c8e8d5ad5e905d45ecec2ce1effb8
Brandon Goding <bgoding@hometap.com>
2025-03-22 16:39:37 -0400

    updated cms url

--- app/www_fortkentoc_org/urls.py
+++ app/www_fortkentoc_org/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
-
+from wagtail import urls as wagtail_urls
 
 urlpatterns = [
     path("", include("website.urls")),
-    path('cms/', include(wagtailadmin_urls)),
-]
+    path('carl/', include(wagtailadmin_urls)),
+    path('', include(wagtail_urls)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
