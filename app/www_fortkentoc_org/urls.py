79f0df11d2b538f3e2f14be4c99620f987834014
Brandon Goding <bgoding@hometap.com>
2025-04-06 21:00:36 -0400

    refactor(settings.py): prod and dev settings

--- app/www_fortkentoc_org/urls.py
+++ app/www_fortkentoc_org/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('carl/', include(wagtailadmin_urls)),
     path('', include(wagtail_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
