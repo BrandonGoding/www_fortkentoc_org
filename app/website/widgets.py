665ebe58c6a18eefc22dce0f09a33ebf05f5c4b1
Brandon H. Goding <brandon.h.goding@gmail.com>
2025-06-25 18:05:02 -0400

    feat(react): setting up for rebuild

--- app/website/widgets.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class OpacitySliderWidget(forms.NumberInput):
-    def __init__(self, attrs=None):
-        final_attrs = {'type': 'range', 'min': 0, 'max': 100, 'step': 5}
-        if attrs:
-            final_attrs.update(attrs)
-        super().__init__(attrs=final_attrs)
